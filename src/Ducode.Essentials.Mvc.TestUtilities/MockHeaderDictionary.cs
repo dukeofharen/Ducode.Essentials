c42ad973042ebd6bfade42454e858fb645922898
Duco <duco@winterwerp.it>
2018-11-07 20:28:06 +0100

    Made MockHeaderDictionary public

--- src/Ducode.Essentials.Mvc.TestUtilities/MockHeaderDictionary.cs
+++ src/Ducode.Essentials.Mvc.TestUtilities/MockHeaderDictionary.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Primitives;
 
 namespace Ducode.Essentials.Mvc.TestUtilities
 {
-   internal class MockHeaderDictionary : Dictionary<string, StringValues>, IHeaderDictionary
+   public class MockHeaderDictionary : Dictionary<string, StringValues>, IHeaderDictionary
    {
       public long? ContentLength { get; set; }
    }
