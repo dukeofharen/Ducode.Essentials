b25e71eda8024834cf41dc495d5bc9f369e965ed
Duco <duco@winterwerp.it>
2018-11-13 19:18:27 +0100

    Added CommonUtils project

---
+++ src/Ducode.Essentials.CommonUtils.Tests/ObjectUtilitiesFacts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ducode.Essentials.CommonUtils.Tests
+{
+   [TestClass]
+   public class ObjectUtilitiesFacts
+   {
+      [TestMethod]
+      public void ObjectUtilities_Set_HappyFlow()
+      {
+         // arrange
+         var list = new List<string>();
+
+         // act
+         list.Set(l => l.Add("test"));
+
+         // assert
+         Assert.AreEqual("test", list.Single());
+      }
+   }
+}
---
+++ src/Ducode.Essentials.CommonUtils/ObjectUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ducode.Essentials.CommonUtils
+{
+   /// <summary>
+   /// A static class containing several methods for working with objects.
+   /// </summary>
+   public static class ObjectUtilities
+   {
+      /// <summary>
+      /// A shorthand method for fluently setting values on an object.
+      /// </summary>
+      /// <typeparam name="T"></typeparam>
+      /// <param name="obj">The object.</param>
+      /// <param name="action">The action.</param>
+      /// <returns>The same object used as input.</returns>
+      public static T Set<T>(this T obj, Action<T> action)
+      {
+         action(obj);
+         return obj;
+      }
+   }
+}
