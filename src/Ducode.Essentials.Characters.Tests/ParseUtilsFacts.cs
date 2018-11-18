c6b4ee41b6f76a1d37ba7d45028789daaf380986
Duco <duco@winterwerp.it>
2018-11-18 19:09:06 +0100

    Added comments to ParseUtils and added unit tests

---
+++ src/Ducode.Essentials.Characters.Tests/ParseUtilsFacts.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ducode.Essentials.Characters.Tests
+{
+   [TestClass]
+   public class ParseUtilsFacts
+   {
+      [TestMethod]
+      public void ParseUtils_SafeParseInt_InputInvalid_ShouldReturnNull()
+      {
+         // arrange
+         string input = "invalid";
+
+         // act
+         var result = ParseUtils.SafeParseInt(input);
+
+         // assert
+         Assert.IsNull(result);
+      }
+
+      [TestMethod]
+      public void ParseUtils_SafeParseInt_HappyFlow()
+      {
+         // arrange
+         string input = "123";
+
+         // act
+         var result = ParseUtils.SafeParseInt(input);
+
+         // assert
+         Assert.AreEqual(123, result);
+      }
+
+      [TestMethod]
+      public void ParseUtils_SafeParseDouble_InputInvalid_ShouldReturnNull()
+      {
+         // arrange
+         string input = "invalid";
+
+         // act
+         var result = ParseUtils.SafeParseDouble(input);
+
+         // assert
+         Assert.IsNull(result);
+      }
+
+      [TestMethod]
+      public void ParseUtils_SafeParseDouble_HappyFlow()
+      {
+         // arrange
+         string input = "123.45";
+
+         // act
+         var result = ParseUtils.SafeParseDouble(input);
+
+         // assert
+         Assert.AreEqual(123.45, result);
+      }
+
+      [TestMethod]
+      public void ParseUtils_SafeParseDouble_WithNumberStylesAndFormatProvider_InputInvalid_ShouldReturnNull()
+      {
+         // arrange
+         string input = "invalid";
+
+         // act
+         var result = ParseUtils.SafeParseDouble(input, NumberStyles.Any, CultureInfo.InvariantCulture);
+
+         // assert
+         Assert.IsNull(result);
+      }
+
+      [TestMethod]
+      public void ParseUtils_SafeParseDouble_WithNumberStylesAndFormatProvider_HappyFlow()
+      {
+         // arrange
+         string input = "123.45";
+
+         // act
+         var result = ParseUtils.SafeParseDouble(input, NumberStyles.Any, CultureInfo.InvariantCulture);
+
+         // assert
+         Assert.AreEqual(123.45, result);
+      }
+
+      [TestMethod]
+      public void ParseUtils_SafeParseDateTime_InputInvalid_ShouldReturnNull()
+      {
+         // arrange
+         string input = "invalid";
+
+         // act
+         var result = ParseUtils.SafeParseDateTime(input, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+         // assert
+         Assert.IsNull(result);
+      }
+
+      [TestMethod]
+      public void ParseUtils_SafeParseDateTime_HappyFlow()
+      {
+         // arrange
+         string input = "29-08-2018";
+         var expectedResult = new DateTime(2018, 8, 29);
+
+         // act
+         var result = ParseUtils.SafeParseDateTime(input, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+         // assert
+         Assert.AreEqual(expectedResult, result);
+      }
+   }
+}
--- src/Ducode.Essentials.Characters/ParseUtils.cs
+++ src/Ducode.Essentials.Characters/ParseUtils.cs
@@ -3,8 +3,16 @@ using System.Globalization;
 
 namespace Ducode.Essentials.Characters
 {
+   /// <summary>
+   /// A static class containing several methods for parsing one type to another.
+   /// </summary>
    public static class ParseUtils
    {
+      /// <summary>
+      /// Safely parses a string to an int. If the string could not be parsed, null is returned.
+      /// </summary>
+      /// <param name="input">The input.</param>
+      /// <returns>An int, or null if the input could not be parsed.</returns>
       public static int? SafeParseInt(string input)
       {
          if (!int.TryParse(input, out int result))
@@ -15,11 +23,25 @@ namespace Ducode.Essentials.Characters
          return result;
       }
 
+      /// <summary>
+      /// Safely parses a string to a double. If the string could not be parsed, null is returned.
+      /// </summary>
+      /// <param name="input">The input.</param>
+      /// <returns>A double, or null if the input could not be parsed.</returns>
       public static double? SafeParseDouble(string input)
       {
          return SafeParseDouble(input, NumberStyles.Any, CultureInfo.InvariantCulture);
       }
 
+      /// <summary>
+      /// Safely parses a string to a double. If the string could not be parsed, null is returned.
+      /// </summary>
+      /// <param name="input">The input.</param>
+      /// <param name="style">The style.</param>
+      /// <param name="provider">The provider.</param>
+      /// <returns>
+      /// A double, or null if the input could not be parsed.
+      /// </returns>
       public static double? SafeParseDouble(string input, NumberStyles style, IFormatProvider provider)
       {
          if (!double.TryParse(input, style, provider, out double result))
@@ -30,6 +52,13 @@ namespace Ducode.Essentials.Characters
          return result;
       }
 
+      /// <summary>
+      /// Safely parses a string to a DateTime. If the string could not be parsed, null is returned.
+      /// </summary>
+      /// <param name="input">The input.</param>
+      /// <param name="format">The format.</param>
+      /// <param name="provider">The provider.</param>
+      /// <returns>A DateTime, or null if the input could not be parsed.</returns>
       public static DateTime? SafeParseDateTime(string input, string format, IFormatProvider provider)
       {
          if (!DateTime.TryParseExact(input, format, provider, DateTimeStyles.None, out var result))
